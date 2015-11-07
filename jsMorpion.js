d68cadd96ca1babb8c0e55cbb4c1e32eadc76b7d
steven <steven.moulinet@gmail.com>
2015-11-07 14:20:42 +0100

    mise en place des couleurs

--- jsMorpion.js
+++ jsMorpion.js
@@ -1,6 +1,10 @@
 /*mise en place du javascript du morpion*/
-function toggle(elemID){
-	var elem = document.getElementById(elemID);
-	elem.addClass("croix");
-	
+function toggle(elemId){
+	var elem = document.getElementById(elemId);
+	if(elem.className== "croix"){
+		elem.className+=" rond";	
+	}
+	else if(elem.className=="rond"){
+		elem.className+=" croix";
+	}
 }
