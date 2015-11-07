b0a6937cbd191d82ae069a6453251bd965700ef9
steven <steven.moulinet@gmail.com>
2015-11-07 18:22:49 +0100

    the end

--- jsMorpion.js
+++ jsMorpion.js
@@ -1,19 +1,18 @@
 /*mise en place du javascript du morpion*/
+var i=0;
 function toggle(elemId){
 	var elem = document.getElementById(elemId);
 	var result;
-	var i=0;
-	if(i==1){
+	
+	if(i%2==1){
 		if(elem.className== "table"){
 			elem.className+=" rond";
 			i++;	
-			alert(i);
 		}
 	}else{
 		if(elem.className=="table"){
 			elem.className+=" croix";
 			i++;
-			alert(i);
 		}
 	}	
 }
